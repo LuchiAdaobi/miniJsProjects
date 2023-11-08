a4682becdf4809df69db1c8b8664d3dfc5971ca3
Luchi <luchichronicles@gmail.com>
2023-11-08 09:11:19 -0800

    creatd toast notification

--- toast-notification/index.js
+++ toast-notification/index.js
@@ -1,1 +1,18 @@
-nb
+const btn = document.getElementById("btn");
+const container = document.getElementById("container");
+
+btn.addEventListener("click", () => {
+  createNotif();
+});
+
+function createNotif() {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.innerText = "Best Day Ever!";
+
+  container.appendChild(notif);
+
+  setTimeout(()=> {
+      notif.remove();
+  }, 3000)
+}
